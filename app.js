3e669c9b5bd0328a106d39d0ce3c23467df3562e
Salt <luffy.w.pirate@gmail.com>
2018-10-12 04:10:11 +0800

    Add EventListener: click and keypress

--- app.js
+++ app.js
@@ -10,5 +10,21 @@ var UIController = (function() {
 
 // Global app controller
 var controller = (function(budgetCtrl, UICtrl) {
-  //
+  document.querySelector(".add__btn").addEventListener("click", function() {
+    // 1. Get the field input data
+    // 2. Add the item to the budget controller
+    // 3. Add the item to the UI
+    // 4. Calculate the budget
+    // 5. Dosplay the budget on the UI
+  });
+
+  document.addEventListener("keypress", function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      // 1. Get the field input data
+      // 2. Add the item to the budget controller
+      // 3. Add the item to the UI
+      // 4. Calculate the budget
+      // 5. Dosplay the budget on the UI
+    }
+  });
 })(budgetController, UIController);
