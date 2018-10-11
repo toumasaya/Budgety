44d5a9f3cfe2de32e5263544eccb538acc9fdcb2
Salt <luffy.w.pirate@gmail.com>
2018-10-12 04:12:20 +0800

    Use ctrlAddItem function to keep DRY

--- app.js
+++ app.js
@@ -10,21 +10,19 @@ var UIController = (function() {
 
 // Global app controller
 var controller = (function(budgetCtrl, UICtrl) {
-  document.querySelector(".add__btn").addEventListener("click", function() {
+  var ctrlAddItem = function() {
     // 1. Get the field input data
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Dosplay the budget on the UI
-  });
+  };
+
+  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", function(event) {
     if (event.keyCode === 13 || event.which === 13) {
-      // 1. Get the field input data
-      // 2. Add the item to the budget controller
-      // 3. Add the item to the UI
-      // 4. Calculate the budget
-      // 5. Dosplay the budget on the UI
+      ctrlAddItem();
     }
   });
 })(budgetController, UIController);
