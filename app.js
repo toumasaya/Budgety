9580303b122a8180789deb075fc88a183e952b33
Salt <luffy.w.pirate@gmail.com>
2018-10-12 10:05:02 +0800

    Updating the percentages: controller

--- app.js
+++ app.js
@@ -224,6 +224,12 @@ var controller = (function(budgetCtrl, UICtrl) {
     UICtrl.displayBudge(budget);
   };
 
+  var updatePercentages = function() {
+    // 1. Calculate percentages
+    // 2. Read percentages form the budget controller
+    // 3. Update the UI with the new percentages
+  };
+
   var ctrlAddItem = function() {
     var input, newItem;
 
@@ -242,6 +248,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
       // 5. Calculate and update budget
       updateBudget();
+
+      // 6. Calculate and update percentages
+      updatePercentages();
     }
   };
 
@@ -263,6 +272,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
       // 3. Update and show the new budget
       updateBudget();
+
+      // 4. Calculate and update percentages
+      updatePercentages();
     }
   };
 
